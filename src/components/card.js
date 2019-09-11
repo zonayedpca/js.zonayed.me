7c9b935222259b9680ef310a4461c39515cca46e
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-11 20:18:07 +0530

    card style done

--- src/components/card.js
+++ src/components/card.js
@@ -3,38 +3,13 @@ import { Link, Router } from 'gatsby'
 
 import { sliceCat } from '../utils'
 
-console.dir(Router)
+import './card.css'
 
 const Card = ({ id, title, index, topic }) => {
   return (
-    <li
-      style={{
-        flexBasis: `calc(100%/3 - 30px)`,
-        margin: `15px`,
-        marginLeft: index % 3 === 0 ? 0 : `15px`,
-        border: `solid 3px #323330`,
-        backgroundColor: `#f0db4f`,
-        borderRadius: 4,
-        display: `flex`,
-        alignItems: `center`,
-      }}
-    >
-      <Link
-        to={`${topic}/${id}`}
-        style={{
-          display: `block`,
-          color: `#323330`,
-          textDecoration: `none`,
-          width: `100%`,
-        }}
-      >
-        <h4
-          style={{
-            padding: `25px`,
-            fontSize: `20px`,
-          }}
-          dangerouslySetInnerHTML={{ __html: sliceCat(title) }}
-        />
+    <li className="card">
+      <Link to={`${topic}/${id}`}>
+        <h4 dangerouslySetInnerHTML={{ __html: sliceCat(title) }} />
       </Link>
     </li>
   )
