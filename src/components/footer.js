44db371545933d96914a6aaeb86691dc9aa62959
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-11 20:24:46 +0530

    css for footer done

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,47 +1,32 @@
 import React from 'react'
 
+import './footer.css'
+
 const Footer = () => {
   return (
-    <footer
-      style={{
-        display: `flex`,
-        flexWrap: `wrap`,
-        alignItems: `center`,
-        padding: `50px 100px`,
-        backgroundColor: `#323330`,
-        color: `#fff`,
-      }}
-    >
-      <div style={{ flex: 1 }} className="foot">
+    <footer className="footer">
+      <div className="foot">
         <div>Image</div>
-        <h3 style={{ marginBottom: `15px` }}>সম্পর্কেঃ</h3>
+        <h3>সম্পর্কেঃ</h3>
         <p>
           প্রোজেক্টটি সম্পূর্ণ সোর্স কোডসহ গিটহাবে রয়েছে। আপনার ভালো লেগে থাকলে
           স্টার দিয়ে আসবেন। আপনার পরামর্শ, মন্তব্য এবং ভুলত্রুটি গিটহাবে ইস্যু
           করে দেওয়ার জন্যে অনুরোধ থাকলো
         </p>
       </div>
-      <div style={{ flex: 1, display: `flex` }} className="foot">
-        <div
-          style={{
-            flex: 1,
-          }}
-        >
-          <h3 style={{ marginBottom: `15px` }}>আপনার জন্যঃ</h3>
-          <ul style={{ listStyle: `none` }}>
+      <div className="foot foot-child">
+        <div className="child">
+          <h3>আপনার জন্যঃ</h3>
+          <ul>
             <li>রিঅ্যাক্ট জেএস শিখুন</li>
             <li>আমি মিডিয়ামে</li>
             <li>আমার ব্লগ</li>
             <li>অন্যকিছু শিখুন</li>
           </ul>
         </div>
-        <div
-          style={{
-            flex: 1,
-          }}
-        >
-          <h3 style={{ marginBottom: `15px` }}>রিসোর্সঃ</h3>
-          <ul style={{ listStyle: `none` }}>
+        <div className="child">
+          <h3>রিসোর্সঃ</h3>
+          <ul>
             <li>এমডিএন ডকুমেন্টেশন</li>
             <li>ইউডেমি কোর্স</li>
             <li>বই</li>
@@ -49,7 +34,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div style={{ flex: 1 }} className="foot">
+      <div className="foot">
         <p>
           <span>❤</span> এর সাথে ডেভেলপ করেছে <a href="#">জুনায়েদ আহমেদ</a>
         </p>
