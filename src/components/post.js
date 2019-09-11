f1c492d3e9e82e50a1cf2ac97ef21a2afb705a65
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-11 20:54:39 +0530

    post css done

--- src/components/post.js
+++ src/components/post.js
@@ -4,23 +4,9 @@ import './post.css'
 
 const Post = ({ title, content }) => {
   return (
-    <div
-      style={{
-        width: `calc(100% - 400px)`,
-        padding: `100px 0`,
-        paddingLeft: `100px`,
-        paddingRight: `50px`,
-      }}
-      className="post"
-    >
+    <div className="post">
       <div>
-        <h2
-          style={{
-            fontSize: `48px`,
-            marginBottom: `25px`,
-          }}
-          dangerouslySetInnerHTML={{ __html: title }}
-        />
+        <h2 className="title" dangerouslySetInnerHTML={{ __html: title }} />
       </div>
       <div>
         <div dangerouslySetInnerHTML={{ __html: content }} />
