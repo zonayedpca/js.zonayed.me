a4cc4b68234cb570b6b8d7483db3cdd123eb4a3e
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-11 21:03:52 +0530

    topic css done

--- src/components/topic.js
+++ src/components/topic.js
@@ -6,54 +6,24 @@ import { sliceCat, topicTranslation } from '../utils'
 import downArrow from '../assets/images/down-arrow.svg'
 import upArrow from '../assets/images/up-arrow.svg'
 
+import './topic.css'
+
 const Topic = ({ category, topics, curCategory, curPost }) => {
   const [open, setOpen] = useState(category === curCategory)
   return (
-    <div style={{ marginBottom: `25px` }}>
-      <h4
-        style={{ fontSize: `22px`, cursor: `pointer` }}
-        onClick={() => setOpen(!open)}
-      >
+    <div className="topic-list">
+      <h4 onClick={() => setOpen(!open)}>
         {topicTranslation(category)}{' '}
-        <img
-          style={{
-            height: `15px`,
-          }}
-          alt="down-arrow"
-          src={open ? upArrow : downArrow}
-        />
+        <img alt="down-arrow" src={open ? upArrow : downArrow} />
       </h4>
       {open && (
-        <ul style={{ marginTop: `25px` }}>
+        <ul>
           {topics.map(({ id, title }) => (
-            <li
-              style={{
-                position: `relative`,
-                fontSize: `18px`,
-                listStyle: `none`,
-                margin: `10px 0`,
-              }}
-              key={id}
-            >
-              {id === Number(curPost) && (
-                <span
-                  style={{
-                    content: `" "`,
-                    position: `absolute`,
-                    height: `100%`,
-                    width: `5px`,
-                    background: `#f0db4f`,
-                    left: `-20px`,
-                  }}
-                />
-              )}
+            <li key={id}>
+              {id === Number(curPost) && <span />}
               <Link
+                className={`${id === Number(curPost) ? `current-post` : ''}`}
                 to={`/${category}/${id}`}
-                style={{
-                  fontWeight: id === Number(curPost) ? `bolder` : `normal`,
-                  color: '#333',
-                  textDecoration: `none`,
-                }}
                 dangerouslySetInnerHTML={{ __html: sliceCat(title.rendered) }}
               />
             </li>
