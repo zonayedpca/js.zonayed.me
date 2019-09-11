89007de6b1216b0b8a8aaaaa1e786fea2091e9dc
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-11 20:38:24 +0530

    header style done

--- src/components/header.js
+++ src/components/header.js
@@ -3,40 +3,24 @@ import { Link } from 'gatsby'
 
 import inverseLogo from '../assets/images/inverse-logo.svg'
 
+import './header.css'
+
 const Header = ({ siteTitle, home }) => {
   return (
-    <header
-      style={{
-        flex: 1,
-      }}
-    >
-      <div
-        style={{
-          display: `flex`,
-          alignItems: `center`,
-          padding: home ? `50px 0` : `25px 0`,
-        }}
-      >
-        <div style={{ flex: 1, margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: `#323330`,
-              textDecoration: `none`,
-            }}
-          >
-            <div style={{ display: `flex`, alignItems: `center` }}>
-              <div
-                style={{ width: `50px`, height: `50px`, marginRight: `15px` }}
-              >
+    <header className="header">
+      <div className={`nav-item ${home ? 'home' : ''}`}>
+        <div className="brand">
+          <Link to="/">
+            <div className="brand-logo">
+              <div className="logo">
                 <img alt="js-logo" src={inverseLogo} />
               </div>
-              <h1 style={{ fontSize: `28px` }}>{siteTitle}</h1>
+              <h1>{siteTitle}</h1>
             </div>
           </Link>
         </div>
-        <div>
-          <ul style={{ display: `flex` }}>
+        <div className="items">
+          <ul>
             <li>Github</li>
             <li>Star</li>
           </ul>
