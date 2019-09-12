f7cb38e04945426ae9dec31edd8c9d5d2ccacfb6
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-12 19:36:51 +0530

    footer done

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,15 +2,31 @@ import React from 'react'
 
 import './footer.css'
 
+import logo from '../assets/images/logo.svg'
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="foot">
-        <div>Image</div>
+      <div className="foot about">
         <h3>সম্পর্কেঃ</h3>
         <p>
-          প্রোজেক্টটি সম্পূর্ণ সোর্স কোডসহ গিটহাবে রয়েছে। আপনার ভালো লেগে থাকলে
-          স্টার দিয়ে আসবেন। আপনার পরামর্শ, মন্তব্য এবং ভুলত্রুটি গিটহাবে ইস্যু
+          প্রোজেক্টটি সম্পূর্ণ সোর্স কোডসহ{' '}
+          <a
+            target="__blank"
+            rel="noopener noreferrer"
+            href="https://github.com/zonayedpca/js.zonayed.me"
+          >
+            গিটহাবে
+          </a>{' '}
+          রয়েছে। আপনার ভালো লেগে থাকলে স্টার দিয়ে আসবেন। আপনার পরামর্শ, মন্তব্য
+          এবং ভুলত্রুটি{' '}
+          <a
+            target="__blank"
+            rel="noopener noreferrer"
+            href="https://github.com/zonayedpca/js.zonayed.me/issues"
+          >
+            গিটহাবে ইস্যু
+          </a>{' '}
           করে দেওয়ার জন্যে অনুরোধ থাকলো
         </p>
       </div>
@@ -18,23 +34,90 @@ const Footer = () => {
         <div className="child">
           <h3>আপনার জন্যঃ</h3>
           <ul>
-            <li>রিঅ্যাক্ট জেএস শিখুন</li>
-            <li>আমি মিডিয়ামে</li>
-            <li>আমার ব্লগ</li>
-            <li>অন্যকিছু শিখুন</li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                রিঅ্যাক্ট জেএস শিখুন
+              </a>
+            </li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                আমি মিডিয়ামে
+              </a>
+            </li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                আমার ব্লগ
+              </a>
+            </li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                অন্যকিছু শিখুন
+              </a>
+            </li>
           </ul>
         </div>
         <div className="child">
           <h3>রিসোর্সঃ</h3>
           <ul>
-            <li>এমডিএন ডকুমেন্টেশন</li>
-            <li>ইউডেমি কোর্স</li>
-            <li>বই</li>
-            <li>অমুক ডকুমেন্টেশন</li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                এমডিএন ডকুমেন্টেশন
+              </a>
+            </li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                ইউডেমি কোর্স
+              </a>
+            </li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                বই
+              </a>
+            </li>
+            <li>
+              <a
+                target="__blank"
+                rel="noopener noreferrer"
+                href="https://github.com/zonayedpca/js.zonayed.me"
+              >
+                অমুক ডকুমেন্টেশন
+              </a>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="foot">
+      <div className="foot copyright">
+        <div>
+          <img src={logo} alt="হাতেকলমে জাভাস্ক্রিপ্ট" />
+        </div>
         <p>
           <span>❤</span> এর সাথে ডেভেলপ করেছে <a href="#">জুনায়েদ আহমেদ</a>
         </p>
