785fa0d6a55f00b88b6421e0804ac0787e2662c4
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-16 19:05:08 +0530

    404 page done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,46 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import error from '../assets/images/error.svg'
+
+import './404.css'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="খুঁজে পাওয়া যায়নি" />
+    <div className="error-area">
+      <div className="img">
+        <img alt="page-not-found" src={error} />
+      </div>
+      <div className="details">
+        <p>
+          আপনি যে পেইজটি খুঁজছেন তা খুঁজে পাওয়া যায়নি। দয়া করে পুনরায় লিঙ্ক চেক
+          করুন অথবা হোমপেজে ফিরে যান।
+        </p>
+      </div>
+      <div className="action">
+        <ul>
+          <li>
+            <Link className="btn btn-home" to="/">
+              হোমপেজ
+            </Link>
+          </li>
+          <li>
+            <a
+              className="btn btn-bug"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/zonayedpca/js.zonayed.me/issues"
+            >
+              বাগ রিপোর্ট
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </Layout>
 )
 
