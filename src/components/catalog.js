0ddba35336ffcf0a3f6a502147f639451bc878cc
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-16 19:56:01 +0530

    661 - 767px Responsiveness done

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Topic from './topic'
+
+import menu from '../assets/images/menu.svg'
 
 import './catalog.css'
 
 const Catalog = ({ path, topics }) => {
+  const [enable, setEnable] = useState(false)
   const [, curCategory, curPost] = path.split('/')
   const categories = Object.keys(topics)
   return (
-    <div className="catalog">
+    <div className={`catalog ${enable ? '' : 'mobile'}`}>
+      <div onClick={() => setEnable(!enable)} className="action">
+        <button>
+          <img src={menu} alt="menu" />
+        </button>
+      </div>
       {categories.map(category => (
         <Topic
           key={category}
