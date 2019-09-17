dd2a3d934092a7aa1749c680b83490608009273e
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-17 18:29:03 +0530

    SEO title and description changed

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -10,7 +10,10 @@ const HomePage = ({ pageContext: { data } }) => {
   const topics = Object.keys(data)
   return (
     <Layout dataTitle={data} home>
-      <SEO title="হাতেকলমে জাভাস্ক্রিপ্ট" />
+      <SEO
+        title="হাতেকলমে জাভাস্ক্রিপ্ট | সম্পূর্ণ বাংলায় জাভাস্ক্রিপ্ট শিখুন"
+        description="সম্পূর্ণ বাংলায় জুনায়েদ এর সাথে জাভাস্ক্রিপ্ট শিখুন। ব্যাসিক থেকে শুরু করে ইএস৬ সিনট্যাক্স এবং নিত্যদিনের প্রয়োজনীয় টেকনিকসহ সবকিছু বাংলায় হাতেকলমে শিখুন"
+      />
       <div className="contents-area">
         {topics.map(topic => (
           <Category key={topic} topic={topic} data={data[topic]} />
