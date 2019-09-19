15bcaf3e6262532bf75debcbb16d533b73a07a04
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-09-19 18:23:27 +0530

    switch added

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 
 import inverseLogo from '../assets/images/inverse-logo.svg'
@@ -8,6 +8,7 @@ import './header.css'
 import github from '../assets/images/github.svg'
 
 const Header = ({ siteTitle, home }) => {
+  const [mode, setMode] = useState(false)
   return (
     <header className="header">
       <div className={`nav-item ${home ? 'home' : ''}`}>
@@ -31,6 +32,12 @@ const Header = ({ siteTitle, home }) => {
               >
                 <img src={github} alt="github" />
               </a>
+            </li>
+            <li>
+              <div
+                onClick={() => setMode(!mode)}
+                className={`switch ${mode && `dark`}`}
+              ></div>
             </li>
           </ul>
         </div>
