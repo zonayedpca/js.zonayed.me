ca62ac9983d99354a2239d7d2b20a5d0901d484c
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-09-23 22:55:13 +0600

    book added

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import Header from './header'
@@ -9,6 +9,12 @@ import Lastread from './lastread'
 
 const Intro = ({ siteTitle, home, dataTitle }) => {
   const [input, setInput] = useState('')
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    if (show) {
+      setShow(false)
+    }
+  }, [])
   return (
     <div className={`intro ${home ? 'home' : ''}`}>
       <Header home={home} siteTitle={siteTitle} />
@@ -35,8 +41,19 @@ const Intro = ({ siteTitle, home, dataTitle }) => {
                 </p>
               </div>
             </div>
-            <div className="intro-book">
-              <p />
+            <div
+              role="button"
+              tabIndex={0}
+              onKeyPress={() => false}
+              onClick={() => setShow(!show)}
+              className="intro-book"
+            >
+              <div className="book-image">
+                <img
+                  alt="Haatekolome JavaScript"
+                  src={require('../assets/images/book.png')}
+                />
+              </div>
             </div>
           </div>
           <div className="action-area">
@@ -48,6 +65,41 @@ const Intro = ({ siteTitle, home, dataTitle }) => {
           </div>
         </>
       )}
+      <div className={`book-info ${show ? 'showed' : ''}`}>
+        <p>
+          <strong>হাতেকলমে জাভাস্ক্রিপ্ট</strong> বইটি এখন পাওয়া যাচ্ছে
+          লাইব্রেরীতে।
+        </p>
+        <hr />
+        <p>
+          সংগ্রহ করুন{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.rokomari.com/book/202772/hate-kolome-javascript"
+          >
+            রকমারি
+          </a>{' '}
+          থেকে। অথবা চাইলে নীলক্ষেত থেকেও [
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/zonayedpca/posts/3381486155264049"
+          >
+            দোকান-১
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/zonayedpca/posts/3384884554924209"
+          >
+            দোকান-২
+          </a>
+          ] সংগ্রহ করতে পারবেন। বাংলাদেশের যেকোনো জায়গায় কুরিয়ারে পাঠানো হয়।
+        </p>
+        <button onClick={() => setShow(false)}>বন্ধ করুন</button>
+      </div>
     </div>
   )
 }
